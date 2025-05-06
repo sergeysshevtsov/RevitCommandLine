8242e4b3507cb322978cf335592522ce46ffc6bc
Sergey Shevtsov <sergey.shevtsov@xyicon.com>
2025-05-06 23:33:19 +0600

    Ribbon data iterate tests

--- source/RevitCommandLine/Application.cs
+++ source/RevitCommandLine/Application.cs
@@ -1,4 +1,7 @@
 ﻿using Autodesk.Revit.UI;
+using Autodesk.Windows;
+using RevitCommandLine.UI.CommandLine.Models;
+using System.Collections.ObjectModel;
 using System.Reflection;
 using System.Windows.Media.Imaging;
 
@@ -27,4 +30,29 @@ public class Application : IExternalApplication
     }
 
     public Result OnShutdown(UIControlledApplication application) => Result.Succeeded;
+
+    ObservableCollection<CommandItem> commandItems = [];
+    public void GetRibbonButtonNames()
+    {
+        var ribbon = ComponentManager.Ribbon;
+        var tabs = ribbon.Tabs;
+        foreach (var tab in tabs)
+        {
+            var panels = tab.Panels;
+            foreach (var panel in panels)
+            {
+                var items = panel.Source.Items;
+                foreach (var item in items)
+                {
+                    if (item is Autodesk.Windows.RibbonButton button)
+                    {
+                        // To research - can we transform button ID into CommandPost item?
+                        //is there any way to get the command post that sent to Revit from the button?
+                        //can we collect full path to the command with Tab->Panel->DropDown button name(if needed)-> button name?
+                        var name = button.Name;
+                    }
+                }
+            }
+        }
+    }
 }
